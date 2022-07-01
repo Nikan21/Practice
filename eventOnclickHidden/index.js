789187bd3225daaed4aea75e8f25347514c2a8ca
Dmitriy <dimonnikanorov2@gmail.com>
2022-07-01 21:20:04 +0300

    Make second simple event

--- eventOnclickHidden/index.js
+++ eventOnclickHidden/index.js
@@ -4,3 +4,9 @@ const button = document.querySelector('button')
 button.onclick = () => {
     text.style.display = 'none'
 }
+
+function hideYourself() {
+    button.hidden = true
+}
+
+button.addEventListener('click', hideYourself)
