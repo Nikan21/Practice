1e79de2c666d1a98bbd563d9a7467eea97363fe1
Dmitriy <dimonnikanorov2@gmail.com>
2022-08-09 10:42:25 +0300

    Create simple animation with cubic Bezie

---
+++ AnimationPlaneWithBezie/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const clickOnImg = (event) => {
+    let endAnimation = false
+
+    const img = event.target
+    img.style.width = '400px'
+    img.style.height = '240px'
+
+}
+
+flyjet.addEventListener('click', clickOnImg)
