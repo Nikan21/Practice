11b459c527fcbc848a9b1fd5e06bb2571db2ffdc
Dmitriy <dimonnikanorov2@gmail.com>
2022-09-13 09:50:01 +0300

    Delete files from other branch

--- jsPracticeTasks/sortWitnIntlCollator.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-let animals = ["тигр", "ёж", "енот", "ехидна", "АИСТ", "ЯК"];
-
-const sort = new Intl.Collator()
-
-animals.sort((a, b) => {
-return sort.compare(a, b)})
-
-console.log(animals)
