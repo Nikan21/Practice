f0f256c0d040aa474f910e80cb8dfe0a32f5a8f9
Dmitriy <dimonnikanorov2@gmail.com>
2022-09-16 13:36:46 +0300

    FIx and add some styles in header panel

--- index.js
+++ index.js
@@ -1,22 +1,64 @@
 "use strict";
+let widthWindow = document.documentElement.clientWidth;
+let heightWindow = document.documentElement.clientHeight;
 
-let widthWindowLG = document.documentElement.clientWidth;
+//Adaptive change header elements
 const ubisoftHeaderLogo = document.querySelector(".navbar-brand");
 const headerWrapperMenu = document.querySelector(".header-wrapper-menu");
 const headerButton = document.querySelector(".header-button");
 
-if (widthWindowLG >= 992) {
+if (widthWindow >= 992) {
   headerWrapperMenu.after(ubisoftHeaderLogo);
 }
 
 window.addEventListener("resize", changeHeader);
 
 function changeHeader(event) {
-  widthWindowLG = document.documentElement.clientWidth;
+  widthWindow = document.documentElement.clientWidth;
+  heightWindow = document.documentElement.clientHeight;
 
-  if (widthWindowLG >= 992) {
+  //Adaptive change header elements
+  if (widthWindow >= 992) {
     headerWrapperMenu.after(ubisoftHeaderLogo);
   } else {
     headerButton.after(ubisoftHeaderLogo);
   }
+}
+
+//Adaptive change size background image in header-menu
+const headerToggler = document.querySelector(".header-toggler");
+const headerMenu = document.querySelector(".header-menu");
+
+headerToggler.addEventListener("click", checkHeaderMenu);
+
+function checkHeaderMenu(event) {
+  let imageWidth = headerMenu.offsetWidth + "px";
+  let imageHeight = headerMenu.offsetHeight - 50 + "px";
+  if (!headerToggler.classList.contains("collapsed")) {
+    headerMenu.style.backgroundSize = `${imageWidth} ${imageHeight}`;
+    window.addEventListener("resize", changeBackgroundHeaderImage);
+  } else {
+    window.removeEventListener("resize", changeBackgroundHeaderImage);
+  }
+
+  function changeBackgroundHeaderImage(event) {
+    console.log(headerToggler);
+    imageWidth = headerMenu.offsetWidth + "px";
+    imageHeight = headerMenu.offsetHeight - 50 + "px";
+    headerMenu.style.backgroundSize = `${imageWidth} ${imageHeight}`;
+  }
+}
+
+if (widthWindow >= 992) {
+  headerToggler.removeEventListener("click", checkHeaderMenu);
+}
+
+//Dynamic style for .modal-backdrop(height)
+headerButton.addEventListener("click", dynamicModalBack);
+
+function dynamicModalBack(event) {
+  const modalBackdrop = document.querySelector(".modal-backdrop");
+  const windowFullHeight = document.documentElement.scrollHeight;
+
+  modalBackdrop.style.height = windowFullHeight + "px";
 }
